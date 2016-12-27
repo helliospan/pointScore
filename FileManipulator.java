ba801edcda2e53ab2e6fea193ca1751274369ee6
helliospan <orbiter782@gmail.com>
2016-12-27 20:41:10 +0300

    Create FileManipulator.java

---
+++ FileManipulator.java
@@ -0,0 +1,24 @@
+package pointScore;
+
+import java.io.*;
+
+/**
+ * Created by Сергей on 27.12.2016.
+ */
+public class FileManipulator {
+
+    FileWriter writer;
+
+    FileManipulator(String fileName){
+        try (FileWriter wr = new FileWriter(fileName, true)){
+            writer = wr;
+        }
+        catch (IOException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    void FileWrite(Goal goal){
+
+    }
+}
