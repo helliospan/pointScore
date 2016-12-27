bf084bf7a8a6907836fd9746f5d90d2033c116e5
helliospan <orbiter782@gmail.com>
2016-12-27 18:02:55 +0300

    Create Goal.java

---
+++ Goal.java
@@ -0,0 +1,15 @@
+package pointScore;
+
+/**
+ * Created by poltaretskiyss on 22/12/2016.
+ */
+public class Goal {
+
+    String title;
+    int points;
+
+    Goal(String title, int points){
+        this.title = title;
+        this.points = points;
+    }
+}
